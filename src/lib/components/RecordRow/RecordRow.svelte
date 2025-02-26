79c20219242e2dc3cfdfee74c5725f04da5117a1
Kyle Shepherd <kyle@thisissoon.com>
2025-02-26 17:30:48 +0000

    feat(Record): Bump letter spacing

--- src/lib/components/RecordRow/RecordRow.svelte
+++ src/lib/components/RecordRow/RecordRow.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="border-b border-white/30 px-4 py-2 first:border-t lg:px-6 lg:py-4">
-	<p class="font-display mb-2 text-xl lg:text-2xl">{record.description}</p>
+	<p class="font-display mb-2 text-xl tracking-wide lg:text-2xl">{record.description}</p>
 	{#if record.info}
 		<p class="text-sm lg:text-base">({record.info})</p>
 	{/if}
